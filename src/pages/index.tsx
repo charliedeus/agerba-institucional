4e72c027f0cf8fa46527884342e19ad351007e72
charliedeus <charliedeus@gmail.com>
2022-07-15 17:09:23 -0300

    Adding a layer in the image hero section

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,7 +5,7 @@ import type { NextPageWithLayout } from './_app'
 const Home: NextPageWithLayout = () => {
   return (
     <section className="w-full h-[calc(100vh-50px)] tablet:h-[calc(100vh-150px)] relative">
-      <div className="w-full h-full flex flex-col justify-end gap-4 px-8 pb-16 absolute top-0">
+      <div className="w-full h-full flex flex-col justify-end gap-4 px-8 pb-16 absolute top-0 z-50">
         <h3 className="font-bold text-white">
           RELATÓRIOS DO PLANO DIRETOR DO SISTEMA DE TRAVESSIA POR TRANSPORTE
           HIDROVIÁRIO DE VALENÇA E ÁREAS DE INFLUÊNCIA
@@ -16,12 +16,13 @@ const Home: NextPageWithLayout = () => {
         </button>
       </div>
 
-      <div className="w-full h-full p-[14px]">
+      <div className="w-full h-full relative p-[14px]">
         <img
           className="w-full h-full object-cover object-center rounded-lg"
           src="https://images.unsplash.com/photo-1509749837427-ac94a2553d0e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=60"
           alt=""
         />
+        <div className="absolute top-0 right-0 bottom-0 left-0 w-full h-full overflow-hidden bg-fixed bg-blue-300 opacity-20"></div>
       </div>
     </section>
   )
