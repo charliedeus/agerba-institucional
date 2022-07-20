240801355b8bbde71a21862768cbe26a96edb144
charliedeus <charliedeus@gmail.com>
2022-07-20 16:08:35 -0300

    Populate a service panel

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -46,42 +46,40 @@ const Home: NextPageWithLayout = () => {
         </Link>
       </section>
 
-      <section className="w-full h-screen p-[14px] m-auto">
-        <h3 className="font-bold text-base uppercase">PAINEL DE SERVIÇOS</h3>
+      <section className="w-full max-w-[1280px] p-[14px] laptop:py-16 m-auto">
+        <h3 className="font-bold text-base laptop:text-2xl laptop:pb-8 uppercase">
+          Painel de Serviços
+        </h3>
 
-        <div className="h-[250px] mt-[0.875rem]">
-          <Swiper
-            className="h-full w-[200px]"
-            slidesPerView={1}
-            centeredSlides={true}
-            spaceBetween={7}
-            grabCursor={true}
-          >
-            <SwiperSlide className="bg-red-300 flex flex-col justify-between p-4">
-              <h3 className="text-base font-bold">Transportes</h3>
+        <div className="flex flex-wrap items-center justify-center gap-4 h-[250px] mt-[0.875rem]">
+          <div className="flex flex-col justify-between w-[200px] h-full p-4 shadow-[2px_2px_8px_rgba(0,0,0,0.25)] shadow-gray-400 laptop:hover:bg-[#3F3F95] laptop:hover:text-[#3F3F95] group">
+            <h3 className="text-base font-bold laptop:group-hover:text-white">
+              Transportes
+            </h3>
 
-              <a
-                className="px-4 py-2 bg-[#3F3F95] bg-opacity-50 rounded-lg"
-                href="#"
-              >
-                <span className="text-sm font-bold leading-tight">
-                  Clique aqui
-                </span>
-              </a>
-            </SwiperSlide>
-            <SwiperSlide className="bg-green-300 flex flex-col justify-between p-4">
-              <h3 className="text-base font-bold">Rodovias</h3>
+            <a
+              className="px-4 py-2 bg-[#3F3F95] laptop:group-hover:bg-white bg-opacity-50 rounded-lg"
+              href="#"
+            >
+              <span className="text-sm font-bold leading-tight">
+                Clique aqui
+              </span>
+            </a>
+          </div>
+          <div className="flex flex-col justify-between w-[200px] h-full p-4 shadow-[2px_2px_8px_rgba(0,0,0,0.25)] shadow-gray-400 laptop:hover:bg-[#3F3F95] laptop:hover:text-[#3F3F95] group">
+            <h3 className="text-base font-bold laptop:group-hover:text-white">
+              Rodovias
+            </h3>
 
-              <a
-                className="px-4 py-2 bg-[#3F3F95] bg-opacity-50 rounded-lg"
-                href="#"
-              >
-                <span className="text-sm font-bold leading-tight">
-                  Clique aqui
-                </span>
-              </a>
-            </SwiperSlide>
-          </Swiper>
+            <a
+              className="px-4 py-2 bg-[#3F3F95] laptop:group-hover:bg-white bg-opacity-50 rounded-lg"
+              href="#"
+            >
+              <span className="text-sm font-bold leading-tight">
+                Clique aqui
+              </span>
+            </a>
+          </div>
         </div>
       </section>
     </>
