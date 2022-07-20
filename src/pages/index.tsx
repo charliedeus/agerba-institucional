09c367dd87610092c8c9c5c698e1b07dd85564e7
charliedeus <charliedeus@gmail.com>
2022-07-20 16:26:09 -0300

    Remove swiper import

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,5 @@
 import type { ReactElement } from 'react'
 import Link from 'next/link'
-import { Swiper, SwiperSlide } from 'swiper/react'
 
 import type { NextPageWithLayout } from './_app'
 
@@ -55,6 +54,118 @@ const Home: NextPageWithLayout = () => {
           <div className="flex flex-col justify-between w-[200px] h-full p-4 shadow-[2px_2px_8px_rgba(0,0,0,0.25)] shadow-gray-400 laptop:hover:bg-[#3F3F95] laptop:hover:text-[#3F3F95] group">
             <h3 className="text-base font-bold laptop:group-hover:text-white">
               Transportes
+            </h3>
+
+            <a
+              className="px-4 py-2 bg-[#3F3F95] laptop:group-hover:bg-white bg-opacity-50 rounded-lg"
+              href="#"
+            >
+              <span className="text-sm font-bold leading-tight">
+                Clique aqui
+              </span>
+            </a>
+          </div>
+          <div className="flex flex-col justify-between w-[200px] h-full p-4 shadow-[2px_2px_8px_rgba(0,0,0,0.25)] shadow-gray-400 laptop:hover:bg-[#3F3F95] laptop:hover:text-[#3F3F95] group">
+            <h3 className="text-base font-bold laptop:group-hover:text-white">
+              Rodovias
+            </h3>
+
+            <a
+              className="px-4 py-2 bg-[#3F3F95] laptop:group-hover:bg-white bg-opacity-50 rounded-lg"
+              href="#"
+            >
+              <span className="text-sm font-bold leading-tight">
+                Clique aqui
+              </span>
+            </a>
+          </div>
+          <div className="flex flex-col justify-between w-[200px] h-full p-4 shadow-[2px_2px_8px_rgba(0,0,0,0.25)] shadow-gray-400 laptop:hover:bg-[#3F3F95] laptop:hover:text-[#3F3F95] group">
+            <h3 className="text-base font-bold laptop:group-hover:text-white">
+              Rodovias
+            </h3>
+
+            <a
+              className="px-4 py-2 bg-[#3F3F95] laptop:group-hover:bg-white bg-opacity-50 rounded-lg"
+              href="#"
+            >
+              <span className="text-sm font-bold leading-tight">
+                Clique aqui
+              </span>
+            </a>
+          </div>
+          <div className="flex flex-col justify-between w-[200px] h-full p-4 shadow-[2px_2px_8px_rgba(0,0,0,0.25)] shadow-gray-400 laptop:hover:bg-[#3F3F95] laptop:hover:text-[#3F3F95] group">
+            <h3 className="text-base font-bold laptop:group-hover:text-white">
+              Rodovias
+            </h3>
+
+            <a
+              className="px-4 py-2 bg-[#3F3F95] laptop:group-hover:bg-white bg-opacity-50 rounded-lg"
+              href="#"
+            >
+              <span className="text-sm font-bold leading-tight">
+                Clique aqui
+              </span>
+            </a>
+          </div>
+          <div className="flex flex-col justify-between w-[200px] h-full p-4 shadow-[2px_2px_8px_rgba(0,0,0,0.25)] shadow-gray-400 laptop:hover:bg-[#3F3F95] laptop:hover:text-[#3F3F95] group">
+            <h3 className="text-base font-bold laptop:group-hover:text-white">
+              Rodovias
+            </h3>
+
+            <a
+              className="px-4 py-2 bg-[#3F3F95] laptop:group-hover:bg-white bg-opacity-50 rounded-lg"
+              href="#"
+            >
+              <span className="text-sm font-bold leading-tight">
+                Clique aqui
+              </span>
+            </a>
+          </div>
+          <div className="flex flex-col justify-between w-[200px] h-full p-4 shadow-[2px_2px_8px_rgba(0,0,0,0.25)] shadow-gray-400 laptop:hover:bg-[#3F3F95] laptop:hover:text-[#3F3F95] group">
+            <h3 className="text-base font-bold laptop:group-hover:text-white">
+              Rodovias
+            </h3>
+
+            <a
+              className="px-4 py-2 bg-[#3F3F95] laptop:group-hover:bg-white bg-opacity-50 rounded-lg"
+              href="#"
+            >
+              <span className="text-sm font-bold leading-tight">
+                Clique aqui
+              </span>
+            </a>
+          </div>
+          <div className="flex flex-col justify-between w-[200px] h-full p-4 shadow-[2px_2px_8px_rgba(0,0,0,0.25)] shadow-gray-400 laptop:hover:bg-[#3F3F95] laptop:hover:text-[#3F3F95] group">
+            <h3 className="text-base font-bold laptop:group-hover:text-white">
+              Rodovias
+            </h3>
+
+            <a
+              className="px-4 py-2 bg-[#3F3F95] laptop:group-hover:bg-white bg-opacity-50 rounded-lg"
+              href="#"
+            >
+              <span className="text-sm font-bold leading-tight">
+                Clique aqui
+              </span>
+            </a>
+          </div>
+          <div className="flex flex-col justify-between w-[200px] h-full p-4 shadow-[2px_2px_8px_rgba(0,0,0,0.25)] shadow-gray-400 laptop:hover:bg-[#3F3F95] laptop:hover:text-[#3F3F95] group">
+            <h3 className="text-base font-bold laptop:group-hover:text-white">
+              Rodovias
+            </h3>
+
+            <a
+              className="px-4 py-2 bg-[#3F3F95] laptop:group-hover:bg-white bg-opacity-50 rounded-lg"
+              href="#"
+            >
+              <span className="text-sm font-bold leading-tight">
+                Clique aqui
+              </span>
+            </a>
+          </div>
+          <div className="flex flex-col justify-between w-[200px] h-full p-4 shadow-[2px_2px_8px_rgba(0,0,0,0.25)] shadow-gray-400 laptop:hover:bg-[#3F3F95] laptop:hover:text-[#3F3F95] group">
+            <h3 className="text-base font-bold laptop:group-hover:text-white">
+              Rodovias
             </h3>
 
             <a
