e1fa910736657dd28e253ef060d7b8dd0855717b
charliedeus <charliedeus@gmail.com>
2022-07-21 14:35:37 -0300

    Adding space in footer items

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -2,15 +2,15 @@ import Link from 'next/link'
 
 export function Footer() {
   return (
-    <footer className="w-full min-h-[300px] flex items-center justify-center bg-[#3F3F95] py-10">
-      <div className="w-full desktop:max-w-[1280px] flex flex-col items-center justify-between gap-10 text-white">
+    <footer className="w-full min-h-[300px] flex items-center justify-center bg-[#3F3F95] py-16">
+      <div className="w-full desktop:max-w-[1280px] flex flex-col desktop:flex-row items-center justify-between gap-10 text-white">
         <Link href="/">
-          <a className="w-full flex items-center justify-center ">
+          <a>
             <img className="max-w-[10rem]" src="/logo-white.png" alt="" />
           </a>
         </Link>
         <div>
-          <ul>
+          <ul className="flex flex-col gap-4">
             <li>Protocolo Online</li>
             <li>Carteira Estudantil</li>
             <li>Arquivos</li>
@@ -19,7 +19,7 @@ export function Footer() {
           </ul>
         </div>
         <div>
-          <ul>
+          <ul className="flex flex-col gap-4">
             <li>Protocolo Online</li>
             <li>Carteira Estudantil</li>
             <li>Arquivos</li>
@@ -28,7 +28,7 @@ export function Footer() {
           </ul>
         </div>
         <div>
-          <ul>
+          <ul className="flex flex-col gap-4">
             <li>Protocolo Online</li>
             <li>Carteira Estudantil</li>
             <li>Arquivos</li>
