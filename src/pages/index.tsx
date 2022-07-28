70bb995ef33d080438323af7c817923473203942
charliedeus <charliedeus@gmail.com>
2022-07-28 11:25:45 -0300

    Remove graphql query in HomePage

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,21 +8,8 @@ import { SuspendedNews } from '../components/Banners/SuspendedNews'
 import { Transparency } from '../components/Banners/Transparency'
 import { SchoolCard } from '../components/Banners/SchoolCard'
 import { ServicesPanel } from '../components/ServicesPanel'
-import { gql, useQuery } from '@apollo/client'
-
-const GET_NOTICIAS_QUERY = gql`
-  query {
-    noticias {
-      Titulo
-    }
-  }
-`
 
 const Home: NextPageWithLayout = () => {
-  const { data } = useQuery(GET_NOTICIAS_QUERY)
-
-  console.log(data)
-
   return (
     <>
       <SuspendedNews />
