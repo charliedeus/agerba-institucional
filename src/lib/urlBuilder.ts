2fbf070e8608ea3b267c572d14bff68bfdd4c4a3
charliedeus <charliedeus@gmail.com>
2022-08-02 15:31:29 -0300

    fix a typescript error in atas do colegiado page

--- src/lib/urlBuilder.ts
+++ src/lib/urlBuilder.ts
@@ -1,4 +1,4 @@
-export const urlBuilder = (src: string) => {
+export const urlBuilder = (src?: string) => {
   const fullUrl = `${process.env.NEXT_PUBLIC_STRAPI_API_BASEURL}${src}`
   return fullUrl
 }
