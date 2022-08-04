21fd6769659482f97cf316c5208dd2404a1e71ac
charliedeus <charliedeus@gmail.com>
2022-08-04 09:17:09 -0300

    Aplying motion effect in herosection

--- src/components/Banners/SuspendedNewes.tsx
+++ src/components/Banners/SuspendedNewes.tsx
@@ -1,17 +1,27 @@
+import { motion } from 'framer-motion'
+
 export function SuspendedNewes() {
   return (
     <>
       <section className="w-full desktop:max-w-[1280px] h-[calc(100vh-70px)] m-auto tablet:h-[calc(100vh-170px)] p-[14px] transition-transform duration-75 ease-in-out">
         <div className="w-full h-full rounded-lg bg-gray-900 overflow-hidden bg-[url('https://images.unsplash.com/photo-1532939163844-547f958e91b4?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=688&q=60')] bg-cover bg-center bg-no-repeat">
           <div className="w-full h-full top-0 left-0 p-[.875rem] ">
-            <div className="w-full h-full flex flex-col laptop:w-1/2 justify-end gap-4 p-[.875rem] rounded-lg text-white laptop:text-gray-900  laptop:pb-24">
-              <h1 className="text-base blur-0 laptop:text-2xl">COMUNICADO</h1>
+            <motion.div
+              initial={{
+                opacity: 0,
+                scale: 0.5,
+              }}
+              animate={{ opacity: 1, scale: 1 }}
+              transition={{ duration: 0.5 }}
+              className="w-full h-full flex flex-col laptop:w-1/2 justify-end gap-4 p-[.875rem] rounded-lg text-white laptop:text-gray-900  laptop:pb-24"
+            >
+              <h1 className="text-base laptop:text-2xl">COMUNICADO</h1>
               <p className="font-bold text-2xl laptop:text-4xl leading-relaxed">
                 Em respeito à legislação eleitoral, a seção de notícias deste
                 site estará suspensa a partir do dia 01 de julho até o fim das
                 eleições 2022.
               </p>
-            </div>
+            </motion.div>
           </div>
         </div>
       </section>
