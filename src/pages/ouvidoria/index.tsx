2993faadeacfc223aff8a9a9d70b966bd5e77b40
charliedeus <charliedeus@gmail.com>
2022-08-04 16:02:52 -0300

    remove no unused vars

--- src/pages/ouvidoria/index.tsx
+++ src/pages/ouvidoria/index.tsx
@@ -3,7 +3,6 @@ import { ReactElement } from 'react'
 import { NextPageWithLayout } from '../_app'
 
 import { DefaultLayout } from '../../layouts/DefaultLayout'
-import { EnvelopeSimple, Phone } from 'phosphor-react'
 
 const OuvidoriaPage: NextPageWithLayout = () => {
   return (
