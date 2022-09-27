3f6927c6aa3e14a0d9f15f6c7ebf2e6db40096b7
charliedeus <charliedeus@gmail.com>
2022-09-27 11:24:15 -0300

    Update nextjs

--- next.config.js
+++ next.config.js
@@ -1,12 +1,14 @@
 /** @type {import('next').NextConfig} */
-const withPWA = require('next-pwa')
+const withPWA = require('next-pwa')({
+  dest: 'public',
+})
+
 const runtimeCaching = require('next-pwa/cache')
 
 const nextConfig = withPWA({
   reactStrictMode: true,
   swcMinify: true,
   pwa: {
-    dest: 'public',
     runtimeCaching,
   },
   output: 'standalone',
